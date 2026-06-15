7de6981289f63b493bfd0c46c8a029f774120995
DevTotoro <totoro.softwaredev@gmail.com>
2026-06-16 00:56:45 +0100

    Remove User entity

--- packages/api/src/main/kotlin/com/github/devtotoro/thevideoclub/api/domain/user/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.github.devtotoro.thevideoclub.api.domain.user
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByExternalId(externalId: String): User?
-}
--- packages/api/src/main/resources/db/migration/V1__initial_setup.sql
+++ packages/api/src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,18 +1,3 @@
-CREATE TABLE users (
-  id BIGINT NOT NULL,
-  external_id VARCHAR(128) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-  -- 
-  CONSTRAINT pk_users PRIMARY KEY (id),
-  -- 
-  CONSTRAINT uq_users_external_id UNIQUE (external_id),
-  CONSTRAINT uq_users_email UNIQUE (email)
-);
-
-CREATE INDEX idx_users_external_id ON users (external_id);
-
 CREATE TABLE media (
   id BIGINT NOT NULL,
   title VARCHAR(255) NOT NULL,
